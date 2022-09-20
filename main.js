2805467638f500eb5be46b0403bb8923e7d4b8d1
Nestor Villca <nestorvillca147@gmail.com>
2022-09-20 18:26:30 -0500

    Se le agrego la clase 7 a la clase 10 para empezar a unir todas las partes del proyecto

---
+++ main.js
@@ -0,0 +1,9 @@
+const menuEmail = document.querySelector('.navbar-email');
+const destopMenu = document.querySelector('.desktop-menu')
+
+menuEmail.addEventListener('click',toggleDesktopMenu);
+
+
+function toggleDesktopMenu(){
+    destopMenu.classList.toggle('inactive')
+}
